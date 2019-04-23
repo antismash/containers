5c8d7f1a053ce6f7cff481c58c0ca8102a910688
Kai Blin <kblin@biosustain.dtu.dk>
2019-04-23 08:46:04 +0200

    standalone: Update  for 5.0.0rc1
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- standalone/publish.sh
+++ standalone/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o errexit
 
-VERSION="4.2.0"
+VERSION=$(cat VERSION)
 
 for img in antismash/standalone:latest antismash/standalone:${VERSION} antismash/standalone-nonfree:latest antismash/standalone-nonfree:${VERSION}; do
     docker push $img
