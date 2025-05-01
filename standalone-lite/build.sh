cc11ba135e01404e8ace1afe019746213d929a7e
Kai Blin <kblin@biosustain.dtu.dk>
2025-05-01 11:37:50 +0200

    standalone-lite: Update for 8.0.0 release
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- standalone-lite/build.sh
+++ standalone-lite/build.sh
@@ -4,7 +4,11 @@ set -o errexit
 VERSION=$(cat VERSION)
 
 buildah build --build-arg "ANTISMASH_VERSION=${VERSION}" -t docker.io/antismash/standalone-lite .
-buildah build --build-arg "ANTISMASH_VERSION=${VERSION}" -t docker.io/antismash/standalone-lite-nonfree -f Dockerfile.nonfree .
 
 buildah tag docker.io/antismash/standalone-lite:latest docker.io/antismash/standalone-lite:${VERSION}
-buildah tag docker.io/antismash/standalone-lite-nonfree:latest docker.io/antismash/standalone-lite-nonfree:${VERSION}
+
+for f in *.tmpl; do
+    OUTFILE="${f/\.tmpl/}"
+    sed -e "s/@@VERSION@@/${VERSION}/" $f > ${OUTFILE}
+    chmod a+x ${OUTFILE}
+done
--- standalone-lite/publish.sh
+++ standalone-lite/publish.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 VERSION=$(cat VERSION)
 
-for img in antismash/standalone-lite:latest antismash/standalone-lite:${VERSION} antismash/standalone-lite-nonfree:latest antismash/standalone-lite-nonfree:${VERSION}; do
+for img in antismash/standalone-lite:latest antismash/standalone-lite:${VERSION}; do
     buildah push docker.io/$img
 done
