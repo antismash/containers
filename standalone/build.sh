7668aee2b87e9251a19fe55d114da79ab8c6f317
Kai Blin <kblin@biosustain.dtu.dk>
2025-05-02 12:13:15 +0200

    standalone: update for antiSMASH 8.0.0
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- standalone/build.sh
+++ standalone/build.sh
@@ -4,7 +4,11 @@ set -o errexit
 VERSION=$(cat VERSION)
 
 buildah build --build-arg "ANTISMASH_VERSION=${VERSION}" -t docker.io/antismash/standalone .
-buildah build --build-arg "ANTISMASH_VERSION=${VERSION}" -t docker.io/antismash/standalone-nonfree -f Dockerfile.nonfree .
 
 buildah tag docker.io/antismash/standalone:latest docker.io/antismash/standalone:${VERSION}
-buildah tag docker.io/antismash/standalone-nonfree:latest docker.io/antismash/standalone-nonfree:${VERSION}
+
+for f in *.tmpl; do
+    OUTFILE="${f/\.tmpl/}"
+    sed -e "s/@@VERSION@@/${VERSION}/" $f > ${OUTFILE}
+    chmod a+x ${OUTFILE}
+done
--- standalone/publish.sh
+++ standalone/publish.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 VERSION=$(cat VERSION)
 
-for img in antismash/standalone:latest antismash/standalone:${VERSION} antismash/standalone-nonfree:latest antismash/standalone-nonfree:${VERSION}; do
+for img in antismash/standalone:latest antismash/standalone:${VERSION}; do
     buildah push docker.io/$img
 done
